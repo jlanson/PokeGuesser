5f73e19a257f6ef7fdbfe4304347c125e0b846d7
Jonathan Lanson <jlanson@uwaterloo.ca>
2024-12-17 23:30:21 -0500

    Added plain pokedex and background dim in pokemongame page

--- src/components/GameModes/Pokemon/PokemonGame.tsx
+++ src/components/GameModes/Pokemon/PokemonGame.tsx
@@ -1,7 +1,87 @@
+import { useEffect, useRef, useState } from "react";
+import Display from "../../Display/Display";
+import Input from "../../UI/Inputs/Input";
+import { generatePokedexIds } from "../../../helpers/generatePokedexIds";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../App";
+import { fetchPokemon, removePokemon as removePokemonFromStore } from "../../../store/slices/gameStateSlice";
+import Autocomplete from "../../UI/Inputs/Autocomplete";
+import "./PokemonGame.css";
+
+export interface Pokemon {
+  id: number;
+  name: string;
+  imgUrl: string;
+}
+
 export default function PokemonGame() {
-    return (
-        <div>
-            <h1>Pokemon Game</h1>
-        </div>
+
+  const dispatch = useDispatch<AppDispatch>();
+  const {generations} = useSelector((state: RootState) => state.gameSettings);
+  const {pokemonPool} = useSelector((state: RootState) => state.gameState);
+
+  let pokedexNumbersRef = useRef<number[]>(generatePokedexIds());
+  const [chosenPokemon, setChosenPokemon] = useState<Pokemon | null>(null);
+  const [win, setWin] = useState<boolean>(false);
+
+  async function loadAndSetPokmemon(){
+    await dispatch(fetchPokemon(generations));
+  }
+
+  function setNewPokemon(){
+    const randomPokemonIndex = Math.floor(Math.random() * pokemonPool.length);
+    setChosenPokemon(pokemonPool[randomPokemonIndex]);
+  }
+
+
+  function removePokemon(){
+    if (chosenPokemon !== null){
+      dispatch(removePokemonFromStore(chosenPokemon.id));
+    }
+    if(pokedexNumbersRef.current.length === 0){
+      setWin(true);
+    } else {
+      setNewPokemon();
+    }
+  }
+
+  useEffect(() => {
+    if(pokemonPool.length === 0){
+      loadAndSetPokmemon();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (pokemonPool.length > 0 && !chosenPokemon) {
+      setNewPokemon();
+    }
+  }, [pokemonPool]);
+  
+  
+  let content = null;
+  let displayContent = null;
+  let inputContent = null;
+  if (chosenPokemon && !win){
+    displayContent = <Display img={{url: chosenPokemon.imgUrl, alt: chosenPokemon.name}} />;
+    //TODO: Choose input component based on settings
+    inputContent = <Input pokemon={chosenPokemon} removePokemon={removePokemon}/>;
+    // content = (
+    //   <div>
+    //     {displayContent}
+    //     <Autocomplete pokemon={chosenPokemon} removePokemon={removePokemon}/>
+    //   </div>
+    // )
+    content = (
+      <div className='pokemon-game'>
+        <img className='pokemon-game-image' src="/src/assets/pokedex.png" alt="" />
+      </div>
     )
+  }else if(win){
+    content = <div>You won!!!</div>
+  }
+  return(
+    <>
+      {content}
+    </>
+  );
 }
--- src/components/PokeguesserApp.tsx
+++ src/components/PokeguesserApp.tsx
@@ -4,10 +4,12 @@ import Game from "./Game.tsx";
 import { MouseEvent } from "react";
 import Header from "./Header.tsx";
 import "./PokeGuesserApp.css";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 
 export default function PokeGuesserApp() {
   const [startGame, setStartGame] = useState(false);
+  const location = useLocation();
+  console.log(location.pathname);
 
   function handleClick(e: MouseEvent<HTMLButtonElement>){
     e.preventDefault();
@@ -22,6 +24,8 @@ export default function PokeGuesserApp() {
 
   return (
     <>
+      <div className="background-container"></div>
+      {location.pathname === "/pokemon" && <div className="background-overlay"></div>}
       <Header/>
       <div className="poke-guesser">
         <Outlet />
